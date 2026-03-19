83a8e90546b1f220c6d3b48784f327009b43d3de
Sudeep-8660 <sudeep.patil@scrobits.com>
2026-03-19 22:14:33 +0530

    updated and create the video call application

--- live-chat/index.js
+++ live-chat/index.js
@@ -8,7 +8,6 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-// Attach socket.io to HTTP server
 const io = new Server(server, {
   cors: {
     origin: '*',
@@ -16,20 +15,37 @@ const io = new Server(server, {
   },
 });
 
-// Socket connection logic
 io.on('connection', (socket) => {
-  console.log('✅ A user connected:', socket.id);
+  console.log('A user connected:', socket.id);
 
   socket.on('join-room', (roomId) => {
     socket.join(roomId);
-    console.log(`👥 ${socket.id} joined room ${roomId}`);
+    console.log(` ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on('offer',({roomId,offer})=>{
+    console.log("offer recieved");
+
+    socket.to(roomId).emit('offer',{offer});
+  });
+
+  socket.on('answer',({roomId,answer})=>{
+    console.log("answer recieved");
+
+    socket.to(roomId).emit('answer',{answer});
+  });
+
+  socket.on('ice-candidate',({roomId,candidate})=>{
+    console.log("ICE condidates recieved",candidate,"from",roomId);
+
+    socket.to(roomId).emit('ice-candidate',{candidate});
   });
 
   /* socket.on('send-message', (data) => {
     console.log('📩 Message received from client:', data); */
 
   socket.on('send-message', ({ roomId, text }) => {
-    console.log(`📩 Message in room ${roomId}:`, text);
+    console.log(`Message in room ${roomId}:`, text);
 
     socket.to(roomId).emit('receive-message', {
       text,
@@ -48,11 +64,10 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('❌ User disconnected:', socket.id);
+    console.log(' User disconnected:', socket.id);
   });
 });
 
-// Simple test route (HTTP)
 app.get('/', (req, res) => {
   res.send('Socket.IO server is running');
 });
